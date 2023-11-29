a13c249fbaaf8936817ced08b972c38d636a9525
Jeff Liu <jeffliu@wifi-10-43-168-57.wifi.berkeley.edu>
2023-11-28 18:23:57 -0800

    pages folder

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>DeFi Code Ninjas Final Project</h1>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
