f63b6376f5398fda72ce58d9964cc17077e45106
Jeff Liu <jeffliu@wifi-10-43-168-57.wifi.berkeley.edu>
2023-11-28 21:18:50 -0800

    added dashboard folder and components

---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { TotalLiquidityBarChart } from "@/app/dashboard/components/total-liquidity-bar-chart"
+
+export default function Dashboard() {
+    return (
+        <div>
+            <TotalLiquidityBarChart />
+        </div>
+    )
+  }
